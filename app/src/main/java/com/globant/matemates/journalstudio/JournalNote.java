f607d78cc8cacb91751f84a5cb556e4c5e16a67c
Ariel Cattáneo <ariel.cattaneo@globant.com>
2015-02-16 19:03:30 -0300

    Added database support for note class.

--- app/src/main/java/com/globant/matemates/journalstudio/JournalNote.java
+++ app/src/main/java/com/globant/matemates/journalstudio/JournalNote.java
@@ -1,14 +1,34 @@
 package com.globant.matemates.journalstudio;
 
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+
 /**
  * Note model for the journal's internal storage.
  *
  * Created by ariel.cattaneo on 16/02/2015.
  */
 public class JournalNote {
+    public final static String ID = "_id";
+    public final static String TITLE = "title";
+    public final static String TEXT = "text";
+    public final static String IMAGE = "image";
+
+    @DatabaseField (generatedId = true, columnName = ID)
+    private int mId;
+
+    @DatabaseField (columnName = TITLE)
     private String mTitle;
+
+    @DatabaseField (columnName = TEXT)
     private String mText;
+
+    @DatabaseField (columnName = IMAGE, dataType = DataType.BYTE_ARRAY)
     private byte[] mImage;
+
+    public JournalNote() {
+
+    }
 
     public String getTitle() {
         return mTitle;
