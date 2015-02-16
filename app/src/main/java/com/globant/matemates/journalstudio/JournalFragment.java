bf0161f79e4d01a807256d6cb9b096814e8c2099
Ariel Cattáneo <ariel.cattaneo@globant.com>
2015-02-16 17:11:16 -0300

    Initial commit.
    The project is created.

---
+++ app/src/main/java/com/globant/matemates/journalstudio/JournalFragment.java
@@ -0,0 +1,23 @@
+package com.globant.matemates.journalstudio;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class JournalFragment extends Fragment {
+
+    public JournalFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_journal, container, false);
+        return rootView;
+    }
+}
